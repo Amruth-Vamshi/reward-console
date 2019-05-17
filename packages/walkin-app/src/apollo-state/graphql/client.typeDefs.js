57f7c09d5d113efd82225e6b067a0720bd5d6b06
abhijeet.getwalk <abhijeet@getwalk.in>
2019-05-17 17:36:21 +0530

    WIP

--- packages/walkin-app/src/apollo-state/graphql/client.typeDefs.js
+++ packages/walkin-app/src/apollo-state/graphql/client.typeDefs.js
@@ -2,6 +2,13 @@ import gql from "graphql-tag";
 
 export const typeDefs = gql`
   extend type Query {
-  
+    isLoggedIn: Boolean!
+    cartItems: [ID!]!
+    locale: String
+    themeType: String
+    navStyle: String
+    pathname: String
+    navCollapsed: Boolean
+    width: Int
   }
 `;
