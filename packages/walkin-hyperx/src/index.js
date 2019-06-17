cfa5ff7a8d8f16fcf50956b0ec0b2b3a0185fc8c
Anvesh Poda <anvesh@getwalk.in>
2019-06-17 07:24:12 +0000

    Revert "Merge branch 'hypeX' into 'master'"
    This reverts merge request !8

--- packages/walkin-hyperx/src/index.js
+++ packages/walkin-hyperx/src/index.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
-import { NEW_CAMPAIGN_BASIC_INFO, NEW_CAMPAIGN_AUDIENCE } from '../src/utils/RouterConstants';
-import BasicInfo from './containers/campaignCreation/basicInfo';
-import Audience from './containers/campaignCreation/audience';
+import React, { Component } from "react";
 
-const App = ({ match }) => {
-	return (
-		<div>
-			<Switch>
-				<Route exact path={'/hyperx'} render={() => <Redirect from="/hyperx" to={NEW_CAMPAIGN_BASIC_INFO} />} />
-				<Route path={NEW_CAMPAIGN_BASIC_INFO} component={BasicInfo} />
-				<Route path="/hyperx/*" component={BasicInfo} />
-			</Switch>
-		</div>
-	);
-};
-
-export default App;
+export default class extends Component {
+  render() {
+    return (
+      <div className="gx-main-content-wrapper">
+        <h2>Welcome to HyperX</h2>
+      </div>
+    );
+  }
+}
