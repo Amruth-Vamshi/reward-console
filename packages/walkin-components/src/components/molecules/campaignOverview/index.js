0d491caf2af8db65e98f5c61d9260fb8376eb74d
Anvesh Poda <anvesh@getwalk.in>
2019-10-11 10:11:21 +0530

    campaign overview changed

--- packages/walkin-components/src/components/molecules/campaignOverview/index.js
+++ packages/walkin-components/src/components/molecules/campaignOverview/index.js
@@ -1,24 +1,28 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Row, Col, Typography } from "antd";
+import { Row, Col, Typography, Progress, Button } from "antd";
 const { Title } = Typography;
 import moment from "moment";
-import './style.css'
+import './overViewStyle.css'
 // import Moment from "react-moment";
 
 export default class Overview extends Component {
+  changeState = e => {
+    console.log(e.target.innerText);
+    if (e.target.innerText == "LAUNCH")
+      this.props.launchCampaign()
+  }
+
   render() {
-    const { campaign, audience, offer, communication } = this.props;
+    const { campaign, audience, offer, communication, view, launchCampaign } = this.props;
     var now = moment();
     var startDate = moment(campaign.startTime);
     var endDate = moment(campaign.endTime);
     var value = "";
     if (now < startDate) {
-      // var diff = startDate.diff(now, "days");
       var diff = moment.duration(now.diff(startDate)).humanize()
       value = "To Start";
     } else if (now < endDate) {
-      // var diff = endDate.diff(now, "days");
       var diff = moment.duration(now.diff(endDate)).humanize()
       value = "To End";
     } else {
@@ -26,26 +30,44 @@ export default class Overview extends Component {
     }
     var start = moment(campaign.startTime).format("DD-MMM-YYYY HH:mm:ss");
     var end = moment(campaign.endTime).format("DD-MMM-YYYY HH:mm:ss");
-    // if (start < end) {
-    //   var diff = start.diff(end);
-    //   console.log("Diff..", diff);
-    // }
     console.log(this.props);
     return (
       <div className="campaignOverview">
-        <Title level={3} className="gx-text-grey"> Overview </Title>
+        {view === false ? <Title level={3} className="gx-text-grey"> Overview </Title> : ''}
         <div style={{ margin: '20px 10px 20px 30px' }}>
 
-          <div className="cpName"> {campaign.name} </div>
+          <Row>
+            <Col style={{ paddingLeft: 0 }} sm={24} md={16}>
+              <div className="cpName"> {campaign.name} </div>
+              <div className="cpDec mb-15">{campaign.description != null ? campaign.description : ""}</div>
+              <div className="cpDaysLeft mb-15"> <b style={{ textTransform: "capitalize" }}>{diff ? diff : ''}</b>  {value} </div>
+            </Col>
+            <Col sm={24} md={8}>
+              <div className='divCenterVertical'>
+                {view ? (campaign.campaignStatus == 'DRAFT' || campaign.campaignStatus == 'LIVE') && value != "Completed" ? <Button shape='round' type='primary'
+                  style={{ width: '200px', letterSpacing: 1, height: 40, fontSize: 20 }} onClick={this.changeState} >
+                  {campaign.campaignStatus == 'DRAFT' ? 'LAUNCH' : campaign.campaignStatus == 'LIVE' ? 'PAUSE' : ''} </Button> : '' : ''}
+              </div>
 
-          <div className="cpDec mb-15">{campaign.description != null ? campaign.description : ""}</div>
-          <div className="cpDaysLeft mb-15"> <b style={{ textTransform: "capitalize" }}>{diff ? diff : ''}</b>  {value} </div>
+            </Col>
+
+          </Row>
+
           <div className="mb-25">
             <Row >
-              <Col md={24} lg={8}>  Start date &nbsp; &nbsp;:&nbsp;&nbsp;&nbsp;{start} </Col>
-              <Col md={24} lg={8}>  End date &nbsp; &nbsp;:&nbsp;&nbsp;&nbsp;{end} </Col>
+              <Col className="mb-10" md={24} lg={8}>  Start date &nbsp; &nbsp;:&nbsp;&nbsp;&nbsp;{start} </Col>
+              <Col className="mb-10" md={24} lg={8}>  End date &nbsp; &nbsp;:&nbsp;&nbsp;&nbsp;{end} </Col>
+              <Col className="mb-10" md={24} lg={8}> <Progress
+                style={{ width: '250px', margin: '0px 5px 0px 5px', colorRendering: '#654665' }}
+                percent={Math.round(
+                  ((moment() - moment(campaign.startTime)) / (moment(campaign.endTime) - moment(campaign.startTime))) * 100)}
+                showInfo={true} status="active"
+              /></Col>
             </Row>
           </div>
+
+
+
 
           {campaign.feedbackForm ?
             <div className="mb-25">
