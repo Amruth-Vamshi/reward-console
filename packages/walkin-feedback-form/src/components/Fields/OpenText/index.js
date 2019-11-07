ea057d38416b0604cad578b94750e26bc62e4a61
abhijeet.getwalk <abhijeet@getwalk.in>
2019-11-07 14:57:35 +0530

    Imports now working

--- packages/walkin-feedback-form/src/components/Fields/OpenText/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const OpenText = () => {
-    return (<h1>Rating Scale</h1>);
-}
-
-export default OpenText;
--- packages/walkin-feedback-form/src/components/Fields/Ranking/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const Ranking = () => {
-    return (<h1>Ranking</h1>);
-}
-
-export default Ranking;
