e6291cdf7b71e6665ea76db751dd2f6131c0ccce
Sachin Thakur <sachin@getwalk.in>
2019-12-03 11:48:48 +0530

    fixed build pipeline issue

--- packages/shared/src/index.tsx
+++ packages/shared/src/index.tsx
@@ -14,8 +14,8 @@ import CampaignFooter from './components/molecules/campaignFooter';
 import Stepper from './components/atoms/stepper';
 import campaignOverview from "./components/molecules/campaignOverview/index"
 import WHeader from "./components/molecules/wHeader/WHeader";
-import FileUpload from "./components/molecules/fileUpload/FileUpload"
-import FileUploader from "./components/molecules/fileUpload/newFileUpload"
+import FileUpload from "./components/molecules/FileUpload/FileUpload"
+import FileUploader from "./components/molecules/FileUpload/newFileUpload"
 import OfferBasicInfoForm from "./components/molecules/offerForm/basicInfo";
 import OfferRedemptionRulesForm from "./components/molecules/offerForm/redemptionRule";
 
