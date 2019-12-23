a32f63a483749488ec7f16172f4d65474ad2bdf0
Anvesh Poda <anvesh@getwalk.in>
2019-12-23 12:26:22 +0530

    entityType changed

--- packages/walkin-hyperx/src/containers/campaign/campaignCreation/index.tsx
+++ packages/walkin-hyperx/src/containers/campaign/campaignCreation/index.tsx
@@ -446,7 +446,7 @@ class CampaignCreation extends Component<IProps, Partial<IState>> {
 		var communicationInput: any = {
 			id: communication.id,
 			entityId: this.state.offerData ? this.state.offerData.id : ' ',
-			entityType: this.state.campaignType == 'OFFER' ? 'Offer' : 'Campaine', //Change to UpperCase
+			entityType: this.state.campaignType == 'OFFER' ? 'OFFER' : 'CAMPAINE',
 			isScheduled: scheduleSaveMark,
 			isRepeatable: scheduleSaveMark,
 			status: DEFAULT_ACTIVE_STATUS,
@@ -487,7 +487,7 @@ class CampaignCreation extends Component<IProps, Partial<IState>> {
 		};
 		var communicationInput: any = {
 			entityId: this.state.campaignType == 'OFFER' ? this.state.offerData.id : this.state.campaign.id,
-			entityType: this.state.campaignType == 'OFFER' ? 'Offer' : 'Campaign',  //Change to UpperCase
+			entityType: this.state.campaignType == 'OFFER' ? 'OFFER' : 'CAMPAINE',
 			campaign_id: this.state.campaign.id,
 			isScheduled: scheduleSaveMark,
 			isRepeatable: scheduleSaveMark,
