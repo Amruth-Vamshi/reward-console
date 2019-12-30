edbefa12977edb242ab3c2d2537a203189593289
sidhu subhash <sidhusubhash@getwalk.in>
2019-12-30 12:42:29 +0530

    api linked to reports rewardx

--- packages/walkin-rewardx/src/containers/Reports/index.tsx
+++ packages/walkin-rewardx/src/containers/Reports/index.tsx
@@ -1,46 +1,72 @@
 import * as React from "react";
+import moment from "moment";
+import {
+  withApollo,
+  graphql,
+  compose,
+  Query,
+  ApolloProviderProps
+} from "react-apollo";
+// import { useQuery } from "@apollo/react-hooks";
+import * as jwt from "jsonwebtoken";
 import { RouteChildrenProps, withRouter } from "react-router";
-import { Card, DatePicker, Button } from "antd";
+
+import { DatePicker, Button, Spin, message } from "antd";
 import "./style.css";
 import Box from "./Box";
+import { REPORT_CONFIGS, REPORTS } from "./../../query";
 
-const data = [
-  {
-    id: 1,
-    name: "Dialy settlement Report",
-    description: "some description about this report",
-    status: "ACTIVE"
-  },
-  {
-    id: 2,
-    name: "Loyalty Points Summary Report",
-    description: "some description about this report",
-    status: "ACTIVE"
-  },
-  {
-    id: 3,
-    name: "Transaction Level Earning Report",
-    description: "some description about this report",
-    status: "ACTIVE"
-  }
-];
-
-interface ReportsProps extends RouteChildrenProps {}
+interface ReportsProps extends RouteChildrenProps, ApolloProviderProps<any> {
+  loading?: boolean;
+  reportConfigs?: any;
+}
 
 interface ReportsState {
-  reports?: any;
+  selectedDates?: any;
+  isFetching?: boolean;
+  activeReportIndex?: any;
 }
 
 class Reports extends React.Component<ReportsProps, ReportsState> {
   constructor(props: ReportsProps) {
     super(props);
-    this.state = {};
+    this.state = {
+      selectedDates: {}
+    };
   }
 
-  getDownloadLink = () => {
-    let url =
-      "http://spatialkeydocs.s3.amazonaws.com/FL_insurance_sample.csv.zip";
-    this.onDownloadFileFromLink(url);
+  getDownloadLink = reportIndex => {
+    this.setState({ isFetching: true, activeReportIndex: reportIndex }, () => {
+      let { selectedDates } = this.state;
+      const { org_id }: any = jwt.decode(localStorage.getItem("jwt"));
+      let inputVariables = {
+        reportDate: selectedDates[reportIndex],
+        reportConfigId: this.props.reportConfigs[reportIndex].id,
+        organizationId: org_id
+      };
+      this.props.client
+        .query({
+          query: REPORTS,
+          variables: inputVariables,
+          fetchPolicy: "network-only"
+        })
+        .then(reportsResponse => {
+          this.setState({ isFetching: false, activeReportIndex: null });
+          if (
+            !reportsResponse.data.reports.length &&
+            !reportsResponse.data.reports.length[0].reportFile.publicUrl
+          ) {
+            message.info("Report not found");
+            return;
+          }
+          this.onDownloadFileFromLink(
+            reportsResponse.data.reports[0].reportFile.publicUrl
+          );
+        })
+        .catch(error => {
+          message.info("Report not found");
+        });
+    });
   };
 
   onDownloadFileFromLink = url => {
@@ -53,18 +79,47 @@ class Reports extends React.Component<ReportsProps, ReportsState> {
     document.body.removeChild(link);
   };
 
-  render() {
-    return (
-      <div className="gx-main-content-wrapper">
-        <div className="reports-flex-row reports-justify-content-space-between margin-bottom-30">
-          <div className="reports-title">Reports</div>
-          <Button className="no-button-bottom-margin reports-auto-schedule-button">
-            AUTO SCHEDULE
-          </Button>
-        </div>
+  onSelectDate = (date, dateString, reportIndex) => {
+    let { selectedDates } = this.state;
+    selectedDates[reportIndex] = dateString;
+    this.setState({ selectedDates });
+  };
+
+  disabledDate = current => {
+    return current && current > moment().endOf("day");
+  };
+
+  rendeReportsList = () => {
+    if (this.props.loading)
+      return (
         <div className={"reports-list-wrapper"}>
-          {data.map((item, index) => (
-            <Box width={"90%"} border={"1px solid #707070"}>
+          <Box
+            width={"90%"}
+            justifyContent={"center"}
+            border={"1px solid #707070"}
+          >
+            <Spin size="large" />
+          </Box>
+        </div>
+      );
+    else if (!this.props.reportConfigs.length)
+      return (
+        <div className={"reports-list-wrapper"}>
+          <Box
+            width={"90%"}
+            justifyContent={"center"}
+            border={"1px solid #707070"}
+            padding={30}
+          >
+            No Reports yet
+          </Box>
+        </div>
+      );
+    else {
+      return (
+        <div className={"reports-list-wrapper"}>
+          {this.props.reportConfigs.map((item, reportIndex) => (
+            <Box key={reportIndex} width={"90%"} border={"1px solid #707070"}>
               <Box
                 width="45%"
                 border="0px solid"
@@ -84,12 +139,22 @@ class Reports extends React.Component<ReportsProps, ReportsState> {
                 maxWidth={280}
               >
                 <span className="reports-description">Select date</span>
-                <DatePicker onChange={() => {}} />
+                <DatePicker
+                  disabledDate={this.disabledDate}
+                  onChange={(date, dateString) => {
+                    this.onSelectDate(date, dateString, reportIndex);
+                  }}
+                />
               </Box>
               <Button
+                disabled={!this.state.selectedDates[reportIndex]}
+                loading={
+                  this.state.isFetching &&
+                  reportIndex === this.state.activeReportIndex
+                }
                 className="no-button-bottom-margin"
                 onClick={() => {
-                  this.getDownloadLink();
+                  this.getDownloadLink(reportIndex);
                 }}
                 type="primary"
               >
@@ -98,9 +163,46 @@ class Reports extends React.Component<ReportsProps, ReportsState> {
             </Box>
           ))}
         </div>
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div className="gx-main-content-wrapper">
+        <div className="reports-flex-row reports-justify-content-space-between margin-bottom-30">
+          <div className="reports-title">Reports</div>
+          <Button
+            disabled
+            className="no-button-bottom-margin reports-auto-schedule-button"
+          >
+            AUTO SCHEDULE
+          </Button>
+        </div>
+        {this.rendeReportsList()}
       </div>
     );
   }
 }
 
-export default withRouter(Reports);
+export default withRouter(
+  compose(
+    graphql(REPORT_CONFIGS, {
+      options: () => {
+        const { org_id }: any = jwt.decode(localStorage.getItem("jwt"));
+        return {
+          variables: {
+            organizationId: org_id
+          },
+          fetchPolicy: "network-only"
+        };
+      },
+      props: ({ data: { loading, error, reportConfigs, refetch } }: any) => ({
+        loading,
+        reportConfigs,
+        error,
+        refetch
+      })
+    })
+  )(withApollo(Reports))
+);
