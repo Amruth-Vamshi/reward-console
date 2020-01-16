ea22dccac7d21316eb01d4990d9e7d910735b048
Kushagra Sinha <kushagra@getwalk.in>
2020-01-16 09:50:49 +0530

    Promo Item component basics

--- packages/walkin-core/src/routes/PromoImageManagement/List.tsx
+++ packages/walkin-core/src/routes/PromoImageManagement/List.tsx
@@ -3,6 +3,9 @@ import { Row, Col, Button, Switch, Icon, Input, DatePicker } from "antd"
 import "./style.css"
 import { History } from 'history'
 import FileUpload from "../Categories/Components/FileUpload"
+import localData from "./data"
+import { element } from "prop-types";
+import moment from "moment"
 
 interface iProps {
     history: History
@@ -10,6 +13,8 @@ interface iProps {
 
 interface iState {
     editPromo: any
+    totalPromo: any
+    originalData: any
 }
 
 class PromoHome extends React.Component<iProps, iState> {
@@ -22,13 +27,33 @@ class PromoHome extends React.Component<iProps, iState> {
                 desc: "",
                 image: "",
                 code: "",
-                status: true
-            }
+                status: true,
+            },
+            originalData: localData,
+            totalPromo: localData
         }
 
     }
 
-    onStatusChange(val) {
+    UNSAFE_componentWillMount() {
+        this.processData()
+    }
+
+    processData() {
+        // localData.forEach((element) => {
+        //     console.log("E : ", element)
+        // })
+    }
+
+    onLinkChange({ target: { value } }, index) {
+        console.log(value, index)
+    }
+
+    onDeepLinkChange({ target: { value } }, index) {
+        console.log(value, index)
+    }
+
+    onStatusChange(val, index) {
         const { editPromo } = this.state
         var data = editPromo
         data.status = val
@@ -43,45 +68,38 @@ class PromoHome extends React.Component<iProps, iState> {
         this.setState({ editPromo: data })
     }
 
-    deletePromo() {
+    deletePromo(element, index) {
         console.log("Delete Promo")
     }
 
-    onStartDateChange(date, dateString) {
+    onStartDateChange(date, dateString, index) {
         console.log(date, dateString);
     }
 
-    onEndDateChange(date, dateString) {
+    onEndDateChange(date, dateString, index) {
         console.log(date, dateString);
     }
 
-    render() {
 
-        const { editPromo } = this.state
-        return (<div>
-            <Row>
-                <Col span={18}>
-                    <div className="promo-header">Promo Images</div>
-                    <div className="promo-subheader">This images show in the app carousel for the defined period of days.</div>
-                </Col>
-                <Col span={6}>
-                    <Button className="addImage">Add Images</Button>
-                </Col>
-            </Row>
-            <div className="promoContainer">
-                <div>
+    renderPromoItem() {
+        const { totalPromo } = this.state
+
+        return (
+            totalPromo.map((promo, index) => {
+                return (<div key={index} className="promoItemContainer">
+                    <div className="promoLine"></div>
                     <Row>
-                        <Col span={18}>Image 1</Col>
+                        <Col span={18}>{promo.name}</Col>
                         <Col span={6}>
                             <Row>
                                 <Col span={12}>
                                     <Row>
-                                        <Switch size="small" onChange={(val) => { this.onStatusChange(val) }} />
-                                        <p className={(editPromo.status == true ? "activeState" : "inActiveState")}>{(editPromo.status == true ? "Active" : "Inactive")}</p>
+                                        <Switch size="small" checked={promo.status} onChange={(val) => { this.onStatusChange(val, index) }} />
+                                        <p className={(promo.status == true ? "activeState" : "inActiveState")}>{(promo.status == true ? "Active" : "Inactive")}</p>
                                     </Row>
                                 </Col>
                                 <Col span={12}>
-                                    <div style={{ textAlign: "center" }} onClick={() => { this.deletePromo() }}><Icon type="delete" theme="filled" /></div>
+                                    <div style={{ textAlign: "center" }} onClick={() => { this.deletePromo(element, index) }}><Icon type="delete" theme="filled" /></div>
                                 </Col>
                             </Row>
                         </Col>
@@ -96,7 +114,11 @@ class PromoHome extends React.Component<iProps, iState> {
                                     <Row className="imageHeading">Image Source :</Row>
                                     <Row style={{ marginTop: "10px" }}>
                                         <Col span={16} style={{ padding: "0px" }}>
-                                            <Input placeholder="http://www.closecececece.com" />
+                                            <Input
+                                                placeholder="url.."
+                                                value={promo.imageUrl}
+                                                onChange={(val) => { this.onLinkChange(val, index) }}
+                                            />
                                         </Col>
                                         <Col span={8} style={{ marginTop: "5px" }}>
                                             <FileUpload uiType={"categoryManagement"} availableImage={0} onImageUpload={this.handleUploadedImage} title="Upload Promo Image" />
@@ -105,7 +127,11 @@ class PromoHome extends React.Component<iProps, iState> {
                                     <Row className="imageHeading">Deep Link (Image Click Action)</Row>
                                     <Row style={{ marginTop: "10px" }}>
                                         <Col span={16} style={{ padding: "0px" }}>
-                                            <Input placeholder="Define Here" />
+                                            <Input
+                                                placeholder="Define Here"
+                                                value={promo.deepLink}
+                                                onChange={(val) => { this.onDeepLinkChange(val, index) }}
+                                            />
                                         </Col>
                                     </Row>
                                 </Col>
@@ -114,15 +140,43 @@ class PromoHome extends React.Component<iProps, iState> {
                         <Col span={6}>
                             <Row className="imageHeading">Start Date:</Row>
                             <Row style={{ marginTop: "10px", paddingBottom: "10px" }}>
-                                <DatePicker style={{ width: "200px" }} onChange={(val, stringVal) => { this.onStartDateChange(val, stringVal) }} />
+                                <DatePicker
+                                    style={{ width: "200px" }}
+                                    value={moment(promo.startDate)}
+                                    onChange={(val, stringVal) => { this.onStartDateChange(val, stringVal, index) }}
+                                />
                             </Row>
                             <Row className="imageHeading">End Date:</Row>
                             <Row style={{ marginTop: "10px" }}>
-                                <DatePicker style={{ width: "200px" }} onChange={(val, stringVal) => { this.onEndDateChange(val, stringVal) }} />
+                                <DatePicker
+                                    style={{ width: "200px" }}
+                                    value={moment(promo.endDate)}
+                                    onChange={(val, stringVal) => { this.onEndDateChange(val, stringVal, index) }}
+                                />
                             </Row>
                         </Col>
                     </Row>
-                </div>
+                </div>)
+            })
+        )
+    }
+
+    render() {
+
+        const { totalPromo } = this.state
+        return (<div>
+            <Row>
+                <Col span={18}>
+                    <div className="promo-header">Promo Images</div>
+                    <div className="promo-subheader">This images show in the app carousel for the defined period of days.</div>
+                </Col>
+                <Col span={6}>
+                    <Button className="addImage">Add Images</Button>
+                </Col>
+            </Row>
+            <div className="promoContainer">
+                {(totalPromo.length > 0) && this.renderPromoItem()}
+                {(totalPromo.length == 0) && <div style={{ textAlign: "center", height: "200px" }}><p style={{ marginTop: "140px", fontSize: "14px", color: "#808080" }}>No Promo available!</p></div>}
             </div>
 
         </div>)
---
+++ packages/walkin-core/src/routes/PromoImageManagement/data.js
@@ -0,0 +1,59 @@
+const data = [
+    {
+        id: "1",
+        name: "Pizza New Year Offer",
+        imageUrl: "https://res.cloudinary.com/servicex-dev/image/upload/v1579080315/servicex/attachments/attachment_1579080313491_pizza_promo.png.png",
+        description: "New year offer generated for 10% off on the whole bill",
+        deepLink: "http://www.pizzahut.ae",
+        status: true,
+        startDate: "2020-01-20",
+        endDate: "2020-02-14"
+
+    },
+    {
+        id: "2",
+        name: "Buy Any medium pizza and get one side free",
+        imageUrl: "https://res.cloudinary.com/servicex-dev/image/upload/v1579080315/servicex/attachments/attachment_1579080313491_pizza_promo.png.png",
+        description: "",
+        deepLink: "",
+        status: true,
+        startDate: "2020-01-20",
+        endDate: "2020-02-14"
+
+    },
+    {
+        id: "3",
+        name: "Buy any 2 regular pizza and get side free",
+        imageUrl: "https://res.cloudinary.com/servicex-dev/image/upload/v1579080315/servicex/attachments/attachment_1579080313491_pizza_promo.png.png",
+        description: "",
+        deepLink: "",
+        status: false,
+        startDate: "2020-01-20",
+        endDate: "2020-02-14"
+
+    },
+    {
+        id: "4",
+        name: "20% off on two sides selection",
+        imageUrl: "https://res.cloudinary.com/servicex-dev/image/upload/v1579080315/servicex/attachments/attachment_1579080313491_pizza_promo.png.png",
+        description: "",
+        deepLink: "",
+        status: true,
+        startDate: "2020-01-20",
+        endDate: "2020-02-14"
+
+    },
+    {
+        id: "5",
+        name: "Offer on large pizza",
+        imageUrl: "https://res.cloudinary.com/servicex-dev/image/upload/v1579080315/servicex/attachments/attachment_1579080313491_pizza_promo.png.png",
+        description: "",
+        deepLink: "",
+        status: true,
+        startDate: "2020-01-20",
+        endDate: "2020-02-14"
+
+    },
+]
+
+export default data
