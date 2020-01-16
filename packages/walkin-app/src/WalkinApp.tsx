af88a02bbf23cb270b975731e6f69b50ca006a1e
Sachin Thakur <sachin@getwalk.in>
2020-01-16 14:28:03 +0530

    removed less file import from console

--- packages/walkin-app/src/WalkinApp.tsx
+++ packages/walkin-app/src/WalkinApp.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 import "@walkinsole/walkin-components/src/assets/vendors/style";
-import "./styles/wieldy.less";
+// import "./styles/wieldy.less";
 import App from "./containers/App/index";
 import { configureClient } from "./appApollo/client/index";
 import { ApolloProvider, Query, Mutation } from "react-apollo";
