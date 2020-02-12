91ca0da5e9f794de9d50000ce53d57cd3b4429dc
abhijeet.getwalk <abhijeet@getwalk.in>
2020-02-12 15:54:40 +0530

    storybook configured

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  stories: ['../src/shared/**/*.stories.(ts|tsx|js|jsx)'],
+  // stories: ['../src/shared/**/*.stories.(ts|tsx|js|jsx)'],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-links',
     {
       name: '@storybook/preset-create-react-app',
-      options: {
-        tsDocgenLoaderOptions: {
-          tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
-        },
-      },
+      // options: {
+      //   tsDocgenLoaderOptions: {
+      //     tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
+      //   },
+      // },
     },
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        configureJSX: true,
-      },
-    },
-    '@storybook/addon-links/register',
+    // {
+    //   name: '@storybook/addon-docs',
+    //   options: {
+    //     configureJSX: true,
+    //   },
+    // },
+    { name: '@storybook/addon-links/register' },
     '@storybook/addon-storysource/register',
     '@storybook/addon-knobs/register',
     '@storybook/addon-viewport/register',
